91d8a40d9b52916046869556fe3f393c531a1dd8
mario <mariojbarra@gmail.com>
2026-01-12 15:22:19 +0100

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/example/hoteltulumresort/HoteltulumresortApplication.java
@@ -0,0 +1,13 @@
+package com.example.hoteltulumresort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoteltulumresortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoteltulumresortApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hoteltulumresort/HoteltulumresortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hoteltulumresort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoteltulumresortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
